557639e2f130ea2ef291d858738e6304bb68b587
maximilian <mengxinnian@gmail.com>
2018-04-23 09:54:48 +0800

    fix post params

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,48 +1,47 @@
 import R from 'ramda'
 import bcrypt from 'bcrypt'
 import jwt from 'jwt-simple'
-import validator from 'validator'
+import isEmail from 'validator/lib/isEmail'
 
 import config from '../config'
 import userModel from '../models/user'
 
 // 注册
 export const signup = async (ctx, next) => {
-  console.log(ctx.query.name)
-  const name = validator.trim(ctx.query.name)
-  const email = validator.trim(ctx.query.email)
-  const pass = validator.trim(ctx.query.pass)
-  const repass = validator.trim(ctx.query.repass)
+  const name = (ctx.request.body.name).trim()
+  const email = (ctx.request.body.email).trim()
+  const pass = (ctx.request.body.pass).trim()
+  const rePass = (ctx.request.body.rePass).trim()
 
   // 验证信息的正确性
-  if (R.any(item => item === '')([name, email, pass, repass])) {
-    ctx.body = {status: 1, msg: '信息不完整'}
+  if (R.any(item => item === '')([name, email, pass, rePass])) {
+    ctx.body = { status: 1, msg: '信息不完整' }
     return
   }
   if (name.length < 3) {
-    ctx.body = {status: 1, msg: '用户名至少需要3个字符'}
+    ctx.body = { status: 1, msg: '用户名至少需要3个字符' }
     return
   }
-  // if (!validator.isEmail(email)) {
+  // if (!isEmail(email)) {
   //   ctx.body = {status: 1, msg: '邮箱不合法'}
   //   return
   // }
-  if (pass !== repass) {
-    ctx.body = {status: 1, msg: '两次密码输入不一致'}
+  if (pass !== rePass) {
+    ctx.body = { status: 1, msg: '两次密码输入不一致' }
     return
   }
 
   //验证用户名是否已注册
   const hadName = await userModel.findOne({name})
   if (hadName || name === 'admin') {
-    ctx.body = {status: 1, msg: '用户名已被注册'}
+    ctx.body = { status: 1, msg: '用户名已被注册' }
     return
   }
 
   //验证邮箱是否已注册
   const hadEmail = await userModel.findOne({email})
   if (hadEmail) {
-    ctx.body = {status: 1, msg: '邮箱已被注册'}
+    ctx.body = { status: 1, msg: '邮箱已被注册' }
     return
   }
 
@@ -52,9 +51,9 @@ export const signup = async (ctx, next) => {
   user.pass = bcrypt.hashSync(pass, 10)
   try {
     await user.save()
-    ctx.body = {status: 0, msg: '注册成功'}
+    ctx.body = { status: 0, msg: '注册成功' }
   } catch (e) {
-    ctx.body = {status: 1,msg: '注册失败，请稍后再试～'}
+    ctx.body = { status: 1,msg: '注册失败，请稍后再试～' }
   }
 
   next()
@@ -63,12 +62,12 @@ export const signup = async (ctx, next) => {
 // 登录
 export const signin = async (ctx, next) => {
   console.log(typeof ctx.query.nameOrEmail)
-  const nameOrEmail = validator.trim(ctx.query.nameOrEmail)
-  const pass = validator.trim(ctx.query.pass)
+  const nameOrEmail = (ctx.query.nameOrEmail).trim()
+  const pass = (ctx.query.pass).trim()
 
   // 验证信息的正确性
   if (R.any(item => item === '')([nameOrEmail, pass])) {
-    ctx.body = {status: 1, msg: '信息不完整'}
+    ctx.body = { status: 1, msg: '信息不完整' }
     return
   }
 
@@ -77,14 +76,14 @@ export const signin = async (ctx, next) => {
     $or: [{name: nameOrEmail}, {email: nameOrEmail}]
   })
   if (!user) {
-    ctx.body = {status: 1, msg: '无此用户'}
+    ctx.body = { status: 1, msg: '无此用户' }
     return
   }
 
   // 验证密码
   const isOk = bcrypt.compareSync(pass, user.pass)
   if (!isOk) {
-    ctx.body = {status: 1, msg: '密码错误'}
+    ctx.body = { status: 1, msg: '密码错误' }
     return
   }
 
@@ -103,13 +102,13 @@ export const signin = async (ctx, next) => {
   }
 
    ctx.cookies.set(config.cookieName, token, opts)
-   ctx.body = {status: 0, msg: '登录成功', name: user.name, token}   //返回token 以备客户端需要
+   ctx.body = { status: 0, msg: '登录成功', name: user.name, token }   //返回token 以备客户端需要
 }
 
 // 登出
 export const signout = async (ctx, next) => {
    ctx.cookies.set(config.cookieName, '', { signed: false, maxAge: 0 })
-   ctx.body = {status: 0, msg: '已登出'}
+   ctx.body = { status: 0, msg: '已登出' }
 }
 
 // 检查登录状态
@@ -128,8 +127,8 @@ export const checklogin = async (ctx, next) => {
 
 // 添加收藏
 export const addCollection = async (ctx, next) => {
-  console.log(ctx.query)
-  const { productId, productImg, productLink } = ctx.query
+  console.log(ctx.request.body)
+  const { productId, productImg, productLink } = ctx.request.body
   const _id = ctx.user.user_id
   const product = { productId, productImg, productLink }
 
